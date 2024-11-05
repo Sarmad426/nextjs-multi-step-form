a83871d72a784962960b016aa3b03c6e349ea25e
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-05 17:19:36 +0500

    Export AuthContext for external usage

--- components/context/AuthContext.tsx
+++ components/context/AuthContext.tsx
@@ -16,7 +16,9 @@ interface AuthContextType {
   logOut: () => Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
