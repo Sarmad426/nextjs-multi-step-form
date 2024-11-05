7085841de27087e165136b298f0f09034b612839
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-05 17:19:53 +0500

    Implemented the sing in functionality

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,26 +1,33 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebaseConfig";
+import { FirebaseError } from "firebase/app";
+import { AuthContext } from "@/components/context/AuthContext";
 
 function Login() {
+  const context = useContext(AuthContext);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log("Trying to login!");
     try {
       await signInWithEmailAndPassword(auth, email, password);
       console.log("User logged in");
-    } catch (error: unknown) {
-      setError(error.message);
+    } catch (error) {
+      if (error instanceof FirebaseError)
+        setError(error?.message || "Something went wrong");
     }
   };
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
+      {context?.user?.email}
       <h2 className="my-12 lg:text-4xl md:text-3xl text-2xl font-semibold">
         Login
       </h2>
